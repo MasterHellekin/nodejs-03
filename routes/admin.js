aebd2b2d8e0b876fef185f48880c9d740df51406
MasterHellekin <52323307+MasterHellekin@users.noreply.github.com>
2020-05-28 11:32:06 -0500

    Initial commit

---
+++ routes/admin.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+const products = [];
+
+router.get('/add-product', (req, res, next) => {
+    // with .pug
+    // res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
+    // with .hbs
+    res.render('add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        formsCSS: true,
+        productCSS: true,
+        activeAddProduct: true
+    });
+});
+
+router.post('/add-product', (req, res) => {
+    products.push({ title: req.body.title });
+    res.redirect('/');
+});
+
+exports.routes = router;
+exports.products = products;
